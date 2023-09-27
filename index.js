404de118d170818a854579090d2b20d91f52775e
Ghassan Ajouri <nghassan1998@gmail.com>
2023-09-27 10:13:24 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cats = ["Milo","Otis","Garfield"];
+const cats=["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendCat(name) {
   cats.push(name);
@@ -17,30 +17,21 @@ function destructivelyRemoveFirstCat() {
 }
 
 function appendCat(name) {
-  // Create a new array based on the original array and the new cat
+ 
   return [...cats, name];
 }
 
 function prependCat(name) {
-  // Create a new array with the new cat followed by the original array
+  
   return [name, ...cats];
 }
 
 function removeLastCat() {
-  // Create a new array that excludes the last cat from the original array
+
   return cats.slice(0, cats.length - 1);
 }
 
 function removeFirstCat() {
-  // Create a new array that excludes the first cat from the original array
+
   return cats.slice(1);
 }
-
-// Example usage:
-destructivelyAppendCat("Ralph");
-destructivelyPrependCat("Bob");
-console.log(cats);
-
-const newCats = appendCat("Fluffy");
-console.log(newCats);
-console.log(cats); // The original cats array remains unchanged.
